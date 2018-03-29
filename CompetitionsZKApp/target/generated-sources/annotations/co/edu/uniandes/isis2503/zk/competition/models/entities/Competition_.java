28a660bb0659fa07033c866c88ef2770c56879ac
LUIS CARLOS GARAVITO ROMERO <lc.garavito@sis.virtual.uniandes.edu.co>
2018-03-28 22:13:30 -0500

    Clean and Build

--- CompetitionsZKApp/target/generated-sources/annotations/co/edu/uniandes/isis2503/zk/competition/models/entities/Competition_.java
+++ CompetitionsZKApp/target/generated-sources/annotations/co/edu/uniandes/isis2503/zk/competition/models/entities/Competition_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-10-28T15:52:16")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-03-28T22:12:37")
 @StaticMetamodel(Competition.class)
 public class Competition_ { 
 
--- CompetitorsZKApp/target/generated-sources/annotations/co/edu/uniandes/isis2503/zk/competitor/models/entities/Competitor_.java
+++ CompetitorsZKApp/target/generated-sources/annotations/co/edu/uniandes/isis2503/zk/competitor/models/entities/Competitor_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-03-28T18:18:53")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-03-28T22:12:35")
 @StaticMetamodel(Competitor.class)
 public class Competitor_ { 
 
